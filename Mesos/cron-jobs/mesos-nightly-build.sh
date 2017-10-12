1a11512bb1c299fc90da82cdc5a26d7194673cf9
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2017-10-12 10:16:17 +0000

    Improve check for already running mesos-nightly-build cron script

--- Mesos/cron-jobs/mesos-nightly-build.sh
+++ Mesos/cron-jobs/mesos-nightly-build.sh
@@ -14,7 +14,8 @@ DIR=$(dirname $0)
 PYTHON_SCRIPT=$(realpath "$DIR/../utils/trigger-gearman-jobs.py") || (echo "ERROR: Failed to get the absolute path for trigger-gearman-jobs.py" && exit 1)
 LOG_FILE=$(realpath "$LOGS_DIR/cron-mesos-nightly-build.log") || (echo "ERROR: Failed to get the absolute path for cron-mesos-nightly-build.log" && exit 1)
 
-ps aux | grep -v " grep " | grep -q "$PYTHON_SCRIPT" && echo -e "$(date +%m-%d-%y-%T) - The script $PYTHON_SCRIPT is already running\n" >> $LOG_FILE && exit 0
+JOB_NAME="mesos-nightly-build"
+ps aux | grep -v " grep " | grep "$PYTHON_SCRIPT" | grep -q "$JOB_NAME" && echo -e "$(date +%m-%d-%y-%T) - The script $PYTHON_SCRIPT for $JOB_NAME is already running\n" >> $LOG_FILE && exit 0
 
 python $PYTHON_SCRIPT -s "$GEARMAN_SERVERS_LIST" -j 'mesos-nightly-build' --params '{"BRANCH": "master"}' 2>&1 >> $LOG_FILE
 
