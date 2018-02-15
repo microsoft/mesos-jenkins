adb317d6117519c458211d2194b3e9b6f7580090
John Kordich <johnkord@microsoft.com>
2018-02-15 11:22:55 -0800

    Refactored DCOS Mesos Path to be in one location for ps1 script.

--- DCOS/templates/preprovision/extensions/preprovision-agent-windows/v1/preprovision-agent-windows.ps1
+++ DCOS/templates/preprovision/extensions/preprovision-agent-windows/v1/preprovision-agent-windows.ps1
@@ -1,12 +1,13 @@
-New-Item -ItemType Directory -Force -Path C:/DCOS/mesos/service/
+$MESOS_SERVICE_DIR = Join-Path $env:SystemDrive "DCOS\mesos\service"
+New-Item -ItemType Directory -Force -Path $MESOS_SERVICE_DIR
 
 $UTF8NoBOM = New-Object System.Text.UTF8Encoding $False
 
 $cred = "{`"principal`": `"mycred1`", `"secret`": `"mysecret1`"}"
-[System.IO.File]::WriteAllLines("C:\DCOS\mesos\service\credential.json", $cred, $UTF8NoBOM)
+[System.IO.File]::WriteAllLines("$MESOS_SERVICE_DIR\credential.json", $cred, $UTF8NoBOM)
 
 $httpCred = "{`"credentials`": [{`"principal`": `"mycred2`", `"secret`": `"mysecret2`"}]}"
-[System.IO.File]::WriteAllLines("C:\DCOS\mesos\service\http_credential.json", $httpCred, $UTF8NoBOM)
+[System.IO.File]::WriteAllLines("$MESOS_SERVICE_DIR\http_credential.json", $httpCred, $UTF8NoBOM)
 
-$serviceEnv = "MESOS_AUTHENTICATE_HTTP_READONLY=true`r`nMESOS_AUTHENTICATE_HTTP_READWRITE=true`r`nMESOS_HTTP_CREDENTIALS=C:\DCOS\mesos\service\http_credential.json`r`nMESOS_CREDENTIAL=C:\DCOS\mesos\service\credential.json"
-[System.IO.File]::WriteAllLines("C:\DCOS\mesos\service\environment-file", $serviceEnv, $UTF8NoBOM)
+$serviceEnv = "MESOS_AUTHENTICATE_HTTP_READONLY=true`r`nMESOS_AUTHENTICATE_HTTP_READWRITE=true`r`nMESOS_HTTP_CREDENTIALS=$MESOS_SERVICE_DIR\http_credential.json`r`nMESOS_CREDENTIAL=$MESOS_SERVICE_DIR\credential.json"
+[System.IO.File]::WriteAllLines("$MESOS_SERVICE_DIR\environment-file", $serviceEnv, $UTF8NoBOM)
