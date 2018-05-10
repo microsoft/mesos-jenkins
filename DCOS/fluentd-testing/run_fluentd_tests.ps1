8ee27c06538f4919e02d49823fe78ce81cda8b81
capsali <mcapsali@gmail.com>
2018-05-10 20:34:41 +0300

    Removed preprovision script from fluentd-testing folder.

--- DCOS/fluentd-testing/run_fluentd_tests.ps1
+++ DCOS/fluentd-testing/run_fluentd_tests.ps1
@@ -1,4 +1,5 @@
-
+$TESTS_DIR = Split-Path -Parent $MyInvocation.MyCommand.Path
+pushd "$TESTS_DIR"
 $results = Invoke-Pester -PassThru
 $FailedTestTally = $results.FailedCount
 if ($FailedTestTally -eq 0) {
@@ -9,3 +10,4 @@ else {
 	write-output "Some tests failed. Number of failed tests is: $FailedTestTally"
 	exit 1
 }
+popd
