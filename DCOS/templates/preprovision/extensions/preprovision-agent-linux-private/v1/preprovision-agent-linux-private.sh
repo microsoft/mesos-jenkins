29710c648169c0d0de29088cb67dd25c555e0f47
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-05-24 10:09:18 -0700

    added post-install script to Linux preprovision

--- DCOS/templates/preprovision/extensions/preprovision-agent-linux-private/v1/preprovision-agent-linux-private.sh
+++ DCOS/templates/preprovision/extensions/preprovision-agent-linux-private/v1/preprovision-agent-linux-private.sh
@@ -28,3 +28,22 @@ Environment=MESOS_AUTHENTICATE_HTTP_READONLY=true
 Environment=MESOS_AUTHENTICATE_HTTP_READWRITE=true
 Environment=MESOS_HTTP_CREDENTIALS=/etc/ethos/dcos-mesos-agent-http-credentials
 Environment=MESOS_CREDENTIAL=/etc/ethos/dcos-mesos-agent-secret" > /etc/systemd/system/dcos-mesos-slave.service.d/10-dcos-mesos-agent-auth.conf
+
+mkdir -p /opt/azure/dcos
+touch /opt/azure/dcos/postinstall.sh
+chmod 744 /opt/azure/dcos/postinstall.sh
+cat > /opt/azure/dcos/postinstall.sh << EOF
+#!/bin/bash
+
+source /opt/azure/containers/provision_source.sh
+
+systemctl stop dcos-metrics-agent.socket
+systemctl disable dcos-metrics-agent.socket
+systemctl stop dcos-metrics-agent.service
+systemctl disable dcos-metrics-agent.service
+
+retrycmd_if_failure 10 10 120 curl -fsSL -o /opt/mesosphere/etc/dcos-diagnostics-runner-config.json https://dcos-mirror.azureedge.net/preprovision/dcos-diagnostics-runner-config-no-dcos-metrics.json
+
+systemctl restart dcos-checks-poststart.service || echo skipped
+EOF
+
