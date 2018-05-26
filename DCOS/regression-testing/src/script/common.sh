522685754095c58a40d8c869b361c90a90d58a21
Dmitry Shmulevich <dmitsh@microsoft.com>
2018-05-26 08:46:11 -0700

    regression: do not use subshell when exiting bash script

--- DCOS/regression-testing/src/script/common.sh
+++ DCOS/regression-testing/src/script/common.sh
@@ -15,12 +15,17 @@ ROOT="${DIR}/.."
 # see: https://github.com/stedolan/jq/issues/105 & https://github.com/stedolan/jq/wiki/FAQ#general-questions
 function jqi() { filename="${1}"; jqexpr="${2}"; jq "${jqexpr}" "${filename}" > "${filename}.tmp" && mv "${filename}.tmp" "${filename}"; }
 
+function exit_with_msg {
+	echo $1
+	exit -1
+}
+
 function generate_template() {
 	# Check pre-requisites
-	[[ ! -z "${INSTANCE_NAME:-}" ]]      || (echo "Must specify INSTANCE_NAME" && exit -1)
-	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || (echo "Must specify CLUSTER_DEFINITION" && exit -1)
-	[[ ! -z "${SSH_KEY:-}" ]]            || (echo "Must specify SSH_KEY" && exit -1)
-	[[ ! -z "${OUTPUT:-}" ]]             || (echo "Must specify OUTPUT" && exit -1)
+	[[ ! -z "${INSTANCE_NAME:-}" ]]      || exit_with_msg "Must specify INSTANCE_NAME"
+	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || exit_with_msg "Must specify CLUSTER_DEFINITION"
+	[[ ! -z "${SSH_KEY:-}" ]]            || exit_with_msg "Must specify SSH_KEY"
+	[[ ! -z "${OUTPUT:-}" ]]             || exit_with_msg "Must specify OUTPUT"
 
 	# Set output directory
 	mkdir -p "${OUTPUT}"
@@ -37,7 +42,7 @@ function generate_template() {
 	jqi "${FINAL_CLUSTER_DEFINITION}" ".properties.linuxProfile.ssh.publicKeys[0].keyData = \"${SSH_PUBLIC_KEY}\""
 
 	if [ "$(jq -r '.properties.windowsProfile' ${FINAL_CLUSTER_DEFINITION})" != "null" ]; then
-		[[ ! -z "${WIN_PWD:-}" ]] || (echo "Must specify WIN_PWD" && exit -1)
+		[[ ! -z "${WIN_PWD:-}" ]] || exit_with_msg "Must specify WIN_PWD"
 		winpwd="$(cat "${WIN_PWD}")"
 		jqi "${FINAL_CLUSTER_DEFINITION}" ".properties.windowsProfile.adminPassword = \"$winpwd\""
 
@@ -58,7 +63,7 @@ function generate_template() {
 
 	orchestratorRelease=$(jq -r '.properties.orchestratorProfile.orchestratorRelease' ${FINAL_CLUSTER_DEFINITION})
 	if [ "$orchestratorRelease" = "" ] ; then
-		[[ ! -z "${ORCHESTRATOR_RELEASE:-}" ]] || (echo "Must specify ORCHESTRATOR_RELEASE" && exit -1)
+		[[ ! -z "${ORCHESTRATOR_RELEASE:-}" ]] || exit_with_msg "Must specify ORCHESTRATOR_RELEASE"
 		jqi "${FINAL_CLUSTER_DEFINITION}" ".properties.orchestratorProfile.orchestratorRelease = \"${ORCHESTRATOR_RELEASE}\""
 	fi
 
@@ -92,11 +97,12 @@ function generate_template() {
 
 function set_azure_account() {
 	# Check pre-requisites
-	[[ ! -z "${SUBSCRIPTION_ID:-}" ]] || (echo "Must specify SUBSCRIPTION_ID" && exit -1)
-	[[ ! -z "${TENANT_ID:-}" ]] || (echo "Must specify TENANT_ID" && exit -1)
-	[[ ! -z "${SERVICE_PRINCIPAL_CLIENT_ID:-}" ]] || (echo "Must specify SERVICE_PRINCIPAL_CLIENT_ID" && exit -1)
-	[[ ! -z "${SERVICE_PRINCIPAL_CLIENT_SECRET:-}" ]] || (echo "Must specify SERVICE_PRINCIPAL_CLIENT_SECRET" && exit -1)
-	which az || (echo "az must be on PATH" && exit -1)
+	[[ ! -z "${SUBSCRIPTION_ID:-}" ]] || exit_with_msg "Must specify SUBSCRIPTION_ID"
+	[[ ! -z "${TENANT_ID:-}" ]]       || exit_with_msg "Must specify TENANT_ID"
+	[[ ! -z "${SERVICE_PRINCIPAL_CLIENT_ID:-}" ]]     || exit_with_msg "Must specify SERVICE_PRINCIPAL_CLIENT_ID"
+	[[ ! -z "${SERVICE_PRINCIPAL_CLIENT_SECRET:-}" ]] || exit_with_msg "Must specify SERVICE_PRINCIPAL_CLIENT_SECRET"
+
+	which az || exit_with_msg "az must be on PATH"
 
 	# Login to Azure-Cli
 	az login --service-principal \
@@ -108,7 +114,7 @@ function set_azure_account() {
 }
 
 function get_secrets() {
-	[[ ! -z "${DATA_DIR:-}" ]] || (echo "Must specify DATA_DIR" && exit -1)
+	[[ ! -z "${DATA_DIR:-}" ]] || exit_with_msg "Must specify DATA_DIR"
 
 	if [[ ! -z "${KEYVAULT_NAME:-}" ]]; then
 		if [[ ! -z "${SSH_KEY_SECRET_NAME:-}" ]]; then
@@ -140,8 +146,8 @@ function get_secrets() {
 }
 
 function create_resource_group() {
-	[[ ! -z "${LOCATION:-}" ]] || (echo "Must specify LOCATION" && exit -1)
-	[[ ! -z "${RESOURCE_GROUP:-}" ]] || (echo "Must specify RESOURCE_GROUP" && exit -1)
+	[[ ! -z "${LOCATION:-}" ]]       || exit_with_msg "Must specify LOCATION"
+	[[ ! -z "${RESOURCE_GROUP:-}" ]] || exit_with_msg "Must specify RESOURCE_GROUP"
 
 	# Create resource group if doesn't exist
 	rg=$(az group show --name="${RESOURCE_GROUP}")
@@ -153,12 +159,12 @@ function create_resource_group() {
 
 function deploy_template() {
 	# Check pre-requisites
-	[[ ! -z "${DEPLOYMENT_NAME:-}" ]] || (echo "Must specify DEPLOYMENT_NAME" && exit -1)
-	[[ ! -z "${LOCATION:-}" ]] || (echo "Must specify LOCATION" && exit -1)
-	[[ ! -z "${RESOURCE_GROUP:-}" ]] || (echo "Must specify RESOURCE_GROUP" && exit -1)
-	[[ ! -z "${OUTPUT:-}" ]] || (echo "Must specify OUTPUT" && exit -1)
+	[[ ! -z "${DEPLOYMENT_NAME:-}" ]] || exit_with_msg "Must specify DEPLOYMENT_NAME"
+	[[ ! -z "${LOCATION:-}" ]]        || exit_with_msg "Must specify LOCATION"
+	[[ ! -z "${RESOURCE_GROUP:-}" ]]  || exit_with_msg "Must specify RESOURCE_GROUP"
+	[[ ! -z "${OUTPUT:-}" ]]          || exit_with_msg "Must specify OUTPUT"
 
-	which az || (echo "az must be on PATH" && exit -1)
+	which az || exit_with_msg "az must be on PATH"
 
 	create_resource_group
 
@@ -172,13 +178,13 @@ function deploy_template() {
 
 function scale_agent_pool() {
 	# Check pre-requisites
-	[[ ! -z "${AGENT_POOL_SIZE:-}" ]] || (echo "Must specify AGENT_POOL_SIZE" && exit -1)
-	[[ ! -z "${DEPLOYMENT_NAME:-}" ]] || (echo "Must specify DEPLOYMENT_NAME" && exit -1)
-	[[ ! -z "${LOCATION:-}" ]] || (echo "Must specify LOCATION" && exit -1)
-	[[ ! -z "${RESOURCE_GROUP:-}" ]] || (echo "Must specify RESOURCE_GROUP" && exit -1)
-	[[ ! -z "${OUTPUT:-}" ]] || (echo "Must specify OUTPUT" && exit -1)
+	[[ ! -z "${AGENT_POOL_SIZE:-}" ]] || exit_with_msg "Must specify AGENT_POOL_SIZE"
+	[[ ! -z "${DEPLOYMENT_NAME:-}" ]] || exit_with_msg "Must specify DEPLOYMENT_NAME"
+	[[ ! -z "${LOCATION:-}" ]]        || exit_with_msg "Must specify LOCATION"
+	[[ ! -z "${RESOURCE_GROUP:-}" ]]  || exit_with_msg "Must specify RESOURCE_GROUP"
+	[[ ! -z "${OUTPUT:-}" ]]          || exit_with_msg "Must specify OUTPUT"
 
-	which az || (echo "az must be on PATH" && exit -1)
+	which az || exit_with_msg "az must be on PATH"
 
 	APIMODEL="${OUTPUT}/apimodel.json"
 	DEPLOYMENT_PARAMS="${OUTPUT}/azuredeploy.parameters.json"
@@ -198,7 +204,7 @@ function scale_agent_pool() {
 }
 
 function get_node_count() {
-	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || (echo "Must specify CLUSTER_DEFINITION" && exit -1)
+	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || exit_with_msg "Must specify CLUSTER_DEFINITION"
 
 	masters=$(jq '.properties.masterProfile.count' ${CLUSTER_DEFINITION})
 	linux_agents=0
@@ -224,7 +230,7 @@ function get_node_count() {
 }
 
 function get_orchestrator_type() {
-	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || (echo "Must specify CLUSTER_DEFINITION" && exit -1)
+	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || exit_with_msg "Must specify CLUSTER_DEFINITION"
 
 	orchestratorType=$(jq -r 'getpath(["properties","orchestratorProfile","orchestratorType"])' ${CLUSTER_DEFINITION} | tr '[:upper:]' '[:lower:]')
 
@@ -232,7 +238,7 @@ function get_orchestrator_type() {
 }
 
 function get_orchestrator_version() {
-	[[ ! -z "${OUTPUT:-}" ]] || (echo "Must specify OUTPUT" && exit -1)
+	[[ ! -z "${OUTPUT:-}" ]] || exit_with_msg "Must specify OUTPUT"
 
 	orchestratorVersion=$(jq -r 'getpath(["properties","orchestratorProfile","orchestratorVersion"])' ${OUTPUT}/apimodel.json)
 	if [[ "$orchestratorVersion" == "null" ]]; then
@@ -243,7 +249,7 @@ function get_orchestrator_version() {
 }
 
 function get_api_version() {
-	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || (echo "Must specify CLUSTER_DEFINITION" && exit -1)
+	[[ ! -z "${CLUSTER_DEFINITION:-}" ]] || exit_with_msg "Must specify CLUSTER_DEFINITION"
 
 	apiVersion=$(jq -r 'getpath(["apiVersion"])' ${CLUSTER_DEFINITION})
 	if [[ "$apiVersion" == "null" ]]; then
@@ -256,10 +262,10 @@ function get_api_version() {
 function validate_agents {
 	OS=$1
 	MARATHON_JSON=$2
-	[[ ! -z "${MARATHON_JSON:-}" ]] || (echo "Marathon JSON filename is not passed" && exit -1)
-	[[ ! -z "${INSTANCE_NAME:-}" ]] || (echo "Must specify INSTANCE_NAME" && exit -1)
-	[[ ! -z "${LOCATION:-}" ]]      || (echo "Must specify LOCATION" && exit -1)
-	[[ ! -z "${SSH_KEY:-}" ]]       || (echo "Must specify SSH_KEY" && exit -1)
+	[[ ! -z "${MARATHON_JSON:-}" ]] || exit_with_msg "Marathon JSON filename is not passed"
+	[[ ! -z "${INSTANCE_NAME:-}" ]] || exit_with_msg "Must specify INSTANCE_NAME"
+	[[ ! -z "${LOCATION:-}" ]]      || exit_with_msg "Must specify LOCATION"
+	[[ ! -z "${SSH_KEY:-}" ]]       || exit_with_msg "Must specify SSH_KEY"
 
 	local remote_exec="ssh -i "${SSH_KEY}" -o ConnectTimeout=30 -o StrictHostKeyChecking=no azureuser@${INSTANCE_NAME}.${LOCATION}.cloudapp.azure.com -p2200"
 	local remote_cp="scp -i "${SSH_KEY}" -P 2200 -o StrictHostKeyChecking=no"
@@ -270,7 +276,7 @@ function validate_agents {
 	echo $(date +%H:%M:%S) "Copying ${MARATHON_JSON} id:$appID instances:$instances"
 
 	${remote_cp} "${ROOT}/${MARATHON_JSON}" azureuser@${INSTANCE_NAME}.${LOCATION}.cloudapp.azure.com:${MARATHON_JSON}
-	[ $? -eq 0 ] || (echo "Error: failed to copy ${MARATHON_JSON}" && exit 1)
+	[ $? -eq 0 ] || exit_with_msg "Error: failed to copy ${MARATHON_JSON}"
 
 	if [[ "$OS" == "Linux" ]]; then
 		local agentFQDN="${INSTANCE_NAME}-lnx.${LOCATION}.cloudapp.azure.com"
@@ -279,7 +285,7 @@ function validate_agents {
 	fi
 
 	${remote_exec} sed -i "s/{agentFQDN}/${agentFQDN}/g" ${MARATHON_JSON}
-	[ $? -eq 0 ] || (echo "Error: failed to configure ${MARATHON_JSON}" && exit 1)
+	[ $? -eq 0 ] || exit_with_msg "Error: failed to configure ${MARATHON_JSON}"
 
 	echo $(date +%H:%M:%S) "Adding marathon app"
 	count=20
@@ -293,7 +299,7 @@ function validate_agents {
 		[ $retval -eq 0 ] && break
 		sleep 15; count=$((count-1))
 	done
-	[ $retval -eq 0 ] || (echo "Error: gave up waiting for marathon to be added" && exit 1)
+	[ $retval -eq 0 ] || exit_with_msg "Error: gave up waiting for marathon to be added"
 
 	# only need to teardown if app added successfully
 	trap "${remote_exec} ./dcos marathon app remove $appID" EXIT
@@ -320,10 +326,6 @@ function validate_agents {
 		exit 1
 	fi
 
-	# install marathon-lb
-	${remote_exec} ./dcos package install marathon-lb --yes
-	[ $? -eq 0 ] || (echo "Error: failed to install marathon-lb" && exit 1)
-
 	# curl webserver through external haproxy
 	echo $(date +%H:%M:%S) "Checking Service"
 	count=20
@@ -331,10 +333,7 @@ function validate_agents {
 		echo $(date +%H:%M:%S) "  ... counting down $count"
 		rc=$(curl -sI --max-time 60 "http://${agentFQDN}" | head -n1 | cut -d$' ' -f2)
 		[[ "$rc" -eq "200" ]] && echo "Successfully hitting simpleweb through external haproxy http://${agentFQDN}" && break
-		if [[ "${count}" -le 1 ]]; then
-			echo "failed to get expected response from nginx through the loadbalancer: Error $rc"
-			exit 1
-		fi
+		[ ${count} -gt 0 ] || exit_with_msg "Error: failed to get expected response from nginx through the loadbalancer ($rc)"
 		sleep 15
 		count=$((count-1))
 	done
@@ -343,34 +342,31 @@ function validate_agents {
 function validate_master_agent_authentication() {
 	echo $(date +%H:%M:%S) "Validating master-agent authentication"
 
-	[[ ! -z "${INSTANCE_NAME:-}" ]]         || (echo "Must specify INSTANCE_NAME" && exit -1)
-	[[ ! -z "${LOCATION:-}" ]]              || (echo "Must specify LOCATION" && exit -1)
-	[[ ! -z "${EXPECTED_MASTER_COUNT:-}" ]] || (echo "Must specify EXPECTED_MASTER_COUNT" && exit -1)
-	[[ ! -z "${SSH_KEY:-}" ]]               || (echo "Must specify SSH_KEY" && exit -1)
+	[[ ! -z "${INSTANCE_NAME:-}" ]]         || exit_with_msg "Must specify INSTANCE_NAME"
+	[[ ! -z "${LOCATION:-}" ]]              || exit_with_msg "Must specify LOCATION"
+	[[ ! -z "${EXPECTED_MASTER_COUNT:-}" ]] || exit_with_msg "Must specify EXPECTED_MASTER_COUNT"
+	[[ ! -z "${SSH_KEY:-}" ]]               || exit_with_msg "Must specify SSH_KEY"
 
 	for i in `seq 0 $(($EXPECTED_MASTER_COUNT - 1))`; do
 		local port=$((i+2200))
 		local remote_exec="ssh -i "${SSH_KEY}" -o ConnectTimeout=30 -o StrictHostKeyChecking=no azureuser@${INSTANCE_NAME}.${LOCATION}.cloudapp.azure.com -p $port"
-		local auth_enabled=$(${remote_exec} 'curl -s http://$(/opt/mesosphere/bin/detect_ip):5050/flags' | jq -r ".flags.authenticate_agents") || {
-			echo "Error: failed to find the Mesos flags on the master $i"
-			exit 1
+
+		auth_enabled=$(${remote_exec} 'curl -s http://$(/opt/mesosphere/bin/detect_ip):5050/flags' | jq -r ".flags.authenticate_agents") || {
+			exit_with_msg "Error: failed to find the Mesos flags on the master $i"
 		}
-		if [[ "$auth_enabled" != "true" ]]; then
-			echo "Error: master $i doesn't have 'authenticate_agents' flag enabled"
-			exit 1
-		fi
+		[[ "$auth_enabled" = "true" ]] || exit_with_msg "Error: master $i doesn't have 'authenticate_agents' flag enabled"
 	done
 	echo $(date +%H:%M:%S) "All masters have the authenticate_agents flag enabled"
 }
 
 function validate() {
-	[[ ! -z "${INSTANCE_NAME:-}" ]]           || (echo "Must specify INSTANCE_NAME" && exit -1)
-	[[ ! -z "${LOCATION:-}" ]]                || (echo "Must specify LOCATION" && exit -1)
-	[[ ! -z "${SSH_KEY:-}" ]]                 || (echo "Must specify SSH_KEY" && exit -1)
-	[[ ! -z "${EXPECTED_NODE_COUNT:-}" ]]     || (echo "Must specify EXPECTED_NODE_COUNT" && exit -1)
-	[[ ! -z "${EXPECTED_LINUX_AGENTS:-}" ]]   || (echo "Must specify EXPECTED_LINUX_AGENTS" && exit -1)
-	[[ ! -z "${EXPECTED_WINDOWS_AGENTS:-}" ]] || (echo "Must specify EXPECTED_WINDOWS_AGENTS" && exit -1)
-	[[ ! -z "${OUTPUT:-}" ]]                  || (echo "Must specify OUTPUT" && exit -1)
+	[[ ! -z "${INSTANCE_NAME:-}" ]]           || exit_with_msg "Must specify INSTANCE_NAME"
+	[[ ! -z "${LOCATION:-}" ]]                || exit_with_msg "Must specify LOCATION"
+	[[ ! -z "${SSH_KEY:-}" ]]                 || exit_with_msg "Must specify SSH_KEY"
+	[[ ! -z "${EXPECTED_NODE_COUNT:-}" ]]     || exit_with_msg "Must specify EXPECTED_NODE_COUNT"
+	[[ ! -z "${EXPECTED_LINUX_AGENTS:-}" ]]   || exit_with_msg "Must specify EXPECTED_LINUX_AGENTS"
+	[[ ! -z "${EXPECTED_WINDOWS_AGENTS:-}" ]] || exit_with_msg "Must specify EXPECTED_WINDOWS_AGENTS"
+	[[ ! -z "${OUTPUT:-}" ]]                  || exit_with_msg "Must specify OUTPUT"
 
 	local remote_exec="ssh -i "${SSH_KEY}" -o ConnectTimeout=30 -o StrictHostKeyChecking=no azureuser@${INSTANCE_NAME}.${LOCATION}.cloudapp.azure.com -p2200"
 
@@ -396,7 +392,7 @@ function validate() {
 		[ $? -eq 0 ] && [ -z "$unhealthy_nodes" ] && echo "All nodes are healthy" && break
 		sleep 30; count=$((count-1))
 	done
-	[[ -z "$unhealthy_nodes" ]] || (echo "Error: unhealthy nodes: $unhealthy_nodes" && exit 1)
+	[[ -z "$unhealthy_nodes" ]] || exit_with_msg "Error: unhealthy nodes: $unhealthy_nodes"
 
 	if [[ "${MASTER_AGENT_AUTHENTICATION:-}" == "true" ]]; then
 		validate_master_agent_authentication
@@ -404,13 +400,17 @@ function validate() {
 
 	echo $(date +%H:%M:%S) "Downloading dcos"
 	${remote_exec} curl -O https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.10/dcos
-	[ $? -eq 0 ] || (echo "Error: failed to download dcos" && exit 1)
+	[ $? -eq 0 ] || exit_with_msg "Error: failed to download dcos"
 	echo $(date +%H:%M:%S) "Setting dcos permissions"
 	${remote_exec} chmod a+x ./dcos
-	[ $? -eq 0 ] || (echo "Error: failed to chmod dcos" && exit 1)
+	[ $? -eq 0 ] || exit_with_msg "Error: failed to chmod dcos"
 	echo $(date +%H:%M:%S) "Configuring dcos"
 	${remote_exec} ./dcos cluster setup http://localhost:80
-	[ $? -eq 0 ] || (echo "Error: failed to configure dcos" && exit 1)
+	[ $? -eq 0 ] || exit_with_msg "Error: failed to configure dcos"
+
+	echo $(date +%H:%M:%S) "Installing marathon-lb"
+	${remote_exec} ./dcos package install marathon-lb --yes
+	[ $? -eq 0 ] || exit_with_msg "Error: failed to install marathon-lb"
 
 	if (( ${EXPECTED_LINUX_AGENTS} > 0 )); then
 		validate_agents "Linux" "nginx-marathon-template.json"
