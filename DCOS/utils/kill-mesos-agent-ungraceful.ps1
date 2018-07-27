63d7fe14992a8c3f59f219b03108439d0ce243ed
Dragos S <39906742+dragoshsch@users.noreply.github.com>
2018-07-27 17:05:31 +0300

    Add detection of service name for private/public windows agents

--- DCOS/utils/kill-mesos-agent-ungraceful.ps1
+++ DCOS/utils/kill-mesos-agent-ungraceful.ps1
@@ -1,34 +1,45 @@
-$mesosServiceObj = Stop-Service dcos-mesos-slave -PassThru
+foreach($name in "dcos-mesos-slave", "dcos-mesos-slave-public") {
+    $svc = Get-Service -Name $name -ErrorAction SilentlyContinue
+    if($svc) {
+        $mesosServiceName = $name
+        break
+    }
+}
+if(!$mesosServiceName) {
+    Throw "Cannot find the Mesos slave agent"
+}
+
+$mesosServiceObj = Stop-Service $mesosServiceName -PassThru
 $mesosServiceObj.WaitForStatus('Stopped','00:00:30')
 if ($mesosServiceObj.Status -ne 'Stopped') {
     Write-Output "Failed to stop the service"
     exit 1
 }
 
-$check = sc.exe qc dcos-mesos-slave | Select-String "--recovery_timeout=1mins"
+$check = sc.exe qc $mesosServiceName | Select-String "--recovery_timeout=1mins"
 if ($LastExitCode -ne 0) {
     Write-Output "Unexpected exit code for sc.exe: $LastExitCode. Aborting"
     exit 1
 }
 if ([string]::IsNullOrEmpty($check)) {
-    $imagePath = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\dcos-mesos-slave' -Name ImagePath).ImagePath
-    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\dcos-mesos-slave' -Name ImagePath -Value "$imagePath --recovery_timeout=1mins"
+    $imagePath = (Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\$mesosServiceName' -Name ImagePath).ImagePath
+    Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\$mesosServiceName' -Name ImagePath -Value "$imagePath --recovery_timeout=1mins"
 }
 
-$mesosServiceObjStart = Start-Service dcos-mesos-slave -PassThru
+$mesosServiceObjStart = Start-Service $mesosServiceName -PassThru
 $mesosServiceObjStart.WaitForStatus('Running','00:00:30')
 if ($mesosServiceObjStart.Status -ne 'Running') {
     Write-Output "Failed to start the service back up"
     exit 1
 } 
 
-$check_after = (sc.exe qc dcos-mesos-slave | Select-String "--recovery_timeout=1mins" | foreach {$_.matches} | select value).Value
+$check_after = (sc.exe qc $mesosServiceName | Select-String "--recovery_timeout=1mins" | foreach {$_.matches} | select value).Value
 if ( $check_after -ne "--recovery_timeout=1mins") {
     Write-Output "Recovery timeout not set to 1min"
     exit 1
 }
 
-sc.exe failure dcos-mesos-slave actions= ////// reset= 86400 2>&1 >$null
+sc.exe failure $mesosServiceName actions= ////// reset= 86400 2>&1 >$null
 if ($LastExitCode -ne 0) {
     Write-Output "Unexpected exit code for sc.exe: $LastExitCode. Aborting"
     exit 1
@@ -36,7 +47,7 @@ if ($LastExitCode -ne 0) {
 
 taskkill /IM mesos-agent.exe /F 2>&1 >$null
 if ($LastExitCode -eq 0) {
-    $mesosServiceObj = Get-Service dcos-mesos-slave
+    $mesosServiceObj = Get-Service $mesosServiceName
     $mesosServiceObj.WaitForStatus('Stopped','00:01:00')
     if ($mesosServiceObj.Status -ne 'Stopped') { 
         Write-Output "FAILURE"
--- DCOS/utils/start-mesos-service.ps1
+++ DCOS/utils/start-mesos-service.ps1
@@ -1,4 +1,15 @@
-$mesosServiceObj = Start-Service dcos-mesos-slave -PassThru
+foreach($name in "dcos-mesos-slave", "dcos-mesos-slave-public") {
+    $svc = Get-Service -Name $name -ErrorAction SilentlyContinue
+    if($svc) {
+        $mesosServiceName = $name
+        break
+    }
+}
+if(!$mesosServiceName) {
+    Throw "Cannot find the Mesos slave agent"
+}
+
+$mesosServiceObj = Start-Service $mesosServiceName -PassThru
 $mesosServiceObj.WaitForStatus('Running','00:00:30')
 if ($mesosServiceObj.Status -ne 'Running') {
     Write-Output "FAILURE"
