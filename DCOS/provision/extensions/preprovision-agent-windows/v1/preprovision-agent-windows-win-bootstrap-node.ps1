587e02a1c980e3f9878f3859f4768d3021846d9d
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2018-09-24 17:58:38 +0000

    Setup Docker with service wrapper in the post-install hook
    Since new deployments upgrade Docker after pre-provision scripts,
    these steps must be executed as part of the post-install scripts.
    Otherwise, they are overwritten by Docker upgrade process.

--- DCOS/provision/extensions/preprovision-agent-windows/v1/preprovision-agent-windows-win-bootstrap-node.ps1
+++ DCOS/provision/extensions/preprovision-agent-windows/v1/preprovision-agent-windows-win-bootstrap-node.ps1
@@ -145,33 +145,6 @@ function Start-CIAgentSetup {
                                -MaxRetryCount 30 -RetryInterval 3 `
                                -RetryMessage "Failed to pre-pull $img Docker image. Retrying"
     }
-    # Enable Docker debug logging and capture stdout and stderr to a file.
-    # We're using the updated service wrapper for this.
-    $serviceName = "Docker"
-    $dockerHome = Join-Path $env:ProgramFiles "Docker"
-    $wrapperUrl = "http://dcos-win.westus2.cloudapp.azure.com/downloads/service-wrapper.exe"
-    Stop-Service $serviceName
-    sc.exe delete $serviceName
-    if($LASTEXITCODE) {
-        Throw "Failed to delete service: $serviceName"
-    }
-    Start-FileDownloadWithCurl -URL $wrapperUrl -Destination "${dockerHome}\service-wrapper.exe" -RetryCount 30
-    $binPath = ("`"${dockerHome}\service-wrapper.exe`" " +
-                "--service-name `"$serviceName`" " +
-                "--exec-start-pre `"powershell.exe if(Test-Path '${env:ProgramData}\docker\docker.pid') { Remove-Item -Force '${env:ProgramData}\docker\docker.pid' }`" " +
-                "--log-file `"$dockerHome\dockerd.log`" " +
-                "`"$dockerHome\dockerd.exe`" -D")
-    New-Service -Name $serviceName -StartupType "Automatic" -Confirm:$false `
-                -DisplayName "Docker Windows Agent" -BinaryPathName $binPath
-    sc.exe failure $serviceName reset=5 actions=restart/1000
-    if($LASTEXITCODE) {
-        Throw "Failed to set $serviceName service recovery options"
-    }
-    sc.exe failureflag $serviceName 1
-    if($LASTEXITCODE) {
-        Throw "Failed to set $serviceName service recovery options"
-    }
-    Start-Service $serviceName
 }
 
 function Install-FluentdAgent () {
